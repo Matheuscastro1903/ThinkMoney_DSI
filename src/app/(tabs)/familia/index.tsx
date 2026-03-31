3aa47f67709121f34e9b14d9ac23c4e4c01c91c3
Pedro Ailton <pedroailton630@gmail.com>
2026-03-31 08:50:12 -0300

    feat: View Inicial

--- src/app/(tabs)/familia/index.tsx
+++ src/app/(tabs)/familia/index.tsx
@@ -1,1 +1,12 @@
 // arquivo destinado a guardar a "primeira tela da família"
+import { View, Text } from 'react-native'
+
+export default function Familia() {
+  return (
+    <View>
+        <View></View>
+        <Text>Bem Vindo À Gestão Familiar</Text>
+        <Text>Você ainda não faz parte de um grupo familiar. Conecte-se com seus entes queridos para gerir o patrimônio em conjunto.</Text>
+    </View>
+  )
+}
