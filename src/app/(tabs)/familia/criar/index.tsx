d7053ace033c8f3655e2d2ff5b1d6fc427f831e1
Pedro Ailton <pedroailton630@gmail.com>
2026-04-10 20:52:46 -0300

    feat(familia): tela criar familia mockada finalizada

--- src/app/(tabs)/familia/criar/index.tsx
+++ src/app/(tabs)/familia/criar/index.tsx
@@ -7,29 +7,34 @@ import {
   View,
 } from "react-native";
 
+import { useState } from "react";
+
 import { useRouter } from "expo-router";
 
-import { MaterialIcons } from '@expo/vector-icons';
+import { MaterialIcons } from "@expo/vector-icons";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Familia() {
   const router = useRouter();
+  const [familyName, setFamilyName] = useState("");
 
-  function abrirSaibaMaisFamilia() {
-    return
+  function gerarCodigo() {
+    return;
+  }
+
+  function copiarCodigo() {
+    return;
   }
 
   function entrarEmFamilia() {
-    return
-  }
-
-  function criarFamilia() {
-    return
+    // Deve haver antes uma verificação se o código foi gerado corretamente e se a família foi criada no banco de dados do Firebase, mas como isso ainda não foi implementado, a função apenas redireciona para a tela inicial da família.
+    // Esse futuro método pode herdar o método de entrar em família do objeto da classe Usuário (Implementação futura)
+    router.push("./home");
+    return;
   }
 
   return (
     <SafeAreaView style={styles.container}>
-
       <View style={styles.header}>
         <View style={styles.familyIcon}>
           <MaterialIcons name="family-restroom" size={50} color="#000000" />
@@ -37,29 +42,58 @@ export default function Familia() {
 
         <Text style={styles.title}>Criar Família</Text>
       </View>
-      
+
       <View style={styles.inputCodeContainer}>
-        <TextInput style={styles.input} placeholder="Digite o código da família" placeholderTextColor="#787581" keyboardType="default"/>
+        <TextInput
+          style={styles.input}
+          placeholder="Digite um nome para a família"
+          placeholderTextColor="#dfdbeb"
+          keyboardType="default"
+          value={familyName}
+          onChangeText={setFamilyName}
+        />
 
-        <TouchableOpacity style={styles.confirmarButton} activeOpacity={0.7} onPress={entrarEmFamilia}>
-          <Text style={styles.textConfirmarButton}>Confirmar e Entrar</Text>
+        <TouchableOpacity
+          style={styles.gerarCodigoButton}
+          activeOpacity={0.7}
+          onPress={gerarCodigo}
+        >
+          <Text style={styles.textGerarCodigoButton}>
+            Gerar código de família
+          </Text>
         </TouchableOpacity>
-
-        <TouchableOpacity style={styles.criarFamiliaButton} activeOpacity={0.7} onPress={criarFamilia}>
-          <Text style={styles.textCriarFamiliaButton}>Criar minha família</Text>
-        </TouchableOpacity>
-
-        <Text>
-          Não tem um código? <Text style={styles.textLink} onPress={abrirSaibaMaisFamilia}>Saiba mais.</Text>
-        </Text>
       </View>
 
       <View style={styles.codeContainer}>
+        <View style={styles.codeContentContainer}>
+          <Text style={styles.codeTitle}>Código de Acesso da Família</Text>
 
+          <Text style={styles.codeText}>THKM-X7R2-89</Text>
+
+          <View style={styles.infoCodeContainer}>
+            <MaterialIcons name="info-outline" size={18} color="#1D1252" />
+            <Text style={styles.infoCodeText}>
+              Compartilhe este código com quem deseja convidar para o grupo.
+            </Text>
+          </View>
+        </View>
+
+        <TouchableOpacity
+          style={styles.copiarCodigoFamiliaButton}
+          activeOpacity={0.7}
+          onPress={copiarCodigo}
+        >
+          <MaterialIcons name="content-copy" size={20} color="#ffffff" />
+        </TouchableOpacity>
       </View>
 
-      <TouchableOpacity style={styles.prontoButton} activeOpacity={0.7} onPress={entrarEmFamilia}>
-
+      <TouchableOpacity
+        style={styles.prontoButton}
+        activeOpacity={0.7}
+        onPress={entrarEmFamilia}
+      >
+        <Text style={styles.textProntoButton}>Pronto</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
@@ -76,7 +110,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     color: "white",
-    marginBottom: 30,
+    marginBottom: 10,
   },
 
   title: {
@@ -91,7 +125,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#ffffff",
     padding: 10,
     borderRadius: 10,
-    marginBottom: 20,
   },
 
   inputCodeContainer: {
@@ -101,63 +134,79 @@ const styles = StyleSheet.create({
     margin: 20,
     borderRadius: 30,
     padding: 30,
+    gap: 20,
   },
 
   input: {
     width: "100%",
-    borderColor: "#ccc",
+    borderColor: "#dfdbeb",
     borderWidth: 1,
-    marginBottom: 10,
+    borderRadius: 5,
     paddingHorizontal: 10,
     paddingVertical: 20,
     fontSize: 18,
   },
 
-  confirmarButton: {
+  gerarCodigoButton: {
     backgroundColor: "#1D1252",
     padding: 20,
-    borderRadius: 5,
-    marginBottom: 20,
+    borderRadius: 20,
     width: "100%",
     alignItems: "center",
     color: "white",
   },
 
-  textConfirmarButton: {
+  textGerarCodigoButton: {
     color: "white",
     fontWeight: "bold",
     fontSize: 18,
-  },
-
-  criarFamiliaButton: {
-    borderColor: "#1D1252",
-    borderWidth: 1,
-    padding: 20,
-    borderRadius: 5,
-    marginBottom: 12,
-    width: "100%",
-    alignItems: "center",
-    color: "#1D1252",
-  },
-
-  textCriarFamiliaButton: {
-    color: "#1D1252",
-    fontWeight: "bold",
-    fontSize: 18,
-  },
-
-  textLink: {
-    color: "#1E90FF",
-    textDecorationLine: "underline",
   },
 
   codeContainer: {
+    flexDirection: "row",
     backgroundColor: "#ffffff",
     marginHorizontal: 20,
     borderRadius: 20,
-    padding: 20,
+    padding: 24,
     alignItems: "center",
-    justifyContent: "center",
+    gap: 20,
+  },
+
+  codeContentContainer: {
+    flex: 1,
+    minWidth: 0,
+    gap: 12,
+  },
+
+  codeTitle: {
+    fontWeight: "regular",
+    textTransform: "uppercase",
+    fontSize: 12,
+  },
+
+  codeText: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+
+  infoCodeContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+
+  infoCodeText: {
+    fontSize: 11,
+    flex: 1,
+    flexShrink: 1,
+  },
+
+  copiarCodigoFamiliaButton: {
+    backgroundColor: "#1D1252",
+    padding: 8,
+    borderRadius: 5,
+    flexShrink: 0,
+    alignSelf: "flex-start",
   },
 
   prontoButton: {
@@ -167,7 +216,13 @@ const styles = StyleSheet.create({
     marginHorizontal: 20,
     marginTop: 20,
     paddingVertical: 16,
-    borderRadius: 10,
+    borderRadius: 20,
     alignItems: "center",
+  },
+
+  textProntoButton: {
+    color: "#ffffff",
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
