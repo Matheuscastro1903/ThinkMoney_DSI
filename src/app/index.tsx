ad263a17e344476882fc8874b51cf2c7798a8610
Leonardo <leonardolafa17@gmail.com>
2026-04-25 14:20:11 -0300

    tirando componentes desnecessarios

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -26,12 +26,7 @@ export default function telaInical(){
 
             {/* botao para teste das telas novas  */}
             
-            <Link href={'/(tabs)/mapa'} asChild>
-                            <TouchableOpacity style={styles.button}>
-                                <Ionicons name="arrow-forward" size={32} color="white" />
-                            </TouchableOpacity>
-
-            </Link>
+           
             
         </View>
     )
