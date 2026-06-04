488348c716121375d5212c27ae1642af9f95f1d1
Matheuscastro1903 <matheusdecastro1903@gmail.com>
2026-06-04 12:39:19 -0300

    fix(navbar): corrigindo rotas do navbar

--- src/components/tabs/familia/navbar/page.tsx
+++ src/components/tabs/familia/navbar/page.tsx
@@ -1,6 +1,6 @@
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
-import { usePathname, Link } from "expo-router";
 import { Ionicons } from '@expo/vector-icons';
+import { Link, usePathname } from "expo-router";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function NavBarFamilia() {
     //pegará o arquivo da tela
@@ -8,54 +8,54 @@ export default function NavBarFamilia() {
 
     return (
         <View style={styles.containerGeral}>
-            <Link href={"/home"} asChild>
-                <TouchableOpacity style={caminho === '/home' ? styles.botaoAtivo : styles.botao}>
+            <Link href={"/familia/home"} asChild>
+                <TouchableOpacity style={caminho?.startsWith('/familia/home') ? styles.botaoAtivo : styles.botao}>
                     <Ionicons
                         name="people"
                         
                         size={14} 
-                        color={caminho === '/home' ? '#ffffff' : '#1D1252'}
+                        color={caminho?.startsWith('/familia/home') ? '#ffffff' : '#1D1252'}
                     />
-                    <Text style={caminho === '/home' ? styles.textAtivo : styles.text}>
+                    <Text style={caminho?.startsWith('/familia/home') ? styles.textAtivo : styles.text}>
                         Família
                     </Text>
                 </TouchableOpacity>
             </Link>
 
-            <Link href={"/"} asChild>
-                <TouchableOpacity style={caminho === '/' ? styles.botaoAtivo : styles.botao}>
+            <Link href={"/familia/editar"} asChild>
+                <TouchableOpacity style={caminho?.startsWith('/familia/editar') ? styles.botaoAtivo : styles.botao}>
                     <Ionicons
                         name="pencil"
                         size={14}
-                        color={caminho === '/' ? '#ffffff' : '#1D1252'}
+                        color={caminho?.startsWith('/familia/editar') ? '#ffffff' : '#1D1252'}
                     />
-                    <Text style={caminho === '/' ? styles.textAtivo : styles.text}>
+                    <Text style={caminho?.startsWith('/familia/editar') ? styles.textAtivo : styles.text}>
                         Editar
                     </Text>
                 </TouchableOpacity>
             </Link>
 
-            <Link href={"/"} asChild>
-                <TouchableOpacity style={caminho === '/' ? styles.botaoAtivo : styles.botao}>
+            <Link href={"/familia/metas"} asChild>
+                <TouchableOpacity style={caminho?.startsWith('/familia/metas') ? styles.botaoAtivo : styles.botao}>
                     <Ionicons
                         name="flag"
                         size={14}
-                        color={caminho === '/' ? '#ffffff' : '#1D1252'}
+                        color={caminho?.startsWith('/familia/metas') ? '#ffffff' : '#1D1252'}
                     />
-                    <Text style={caminho === '/' ? styles.textAtivo : styles.text}>
+                    <Text style={caminho?.startsWith('/familia/metas') ? styles.textAtivo : styles.text}>
                         Metas
                     </Text>
                 </TouchableOpacity>
             </Link>
 
-            <Link href={"/"} asChild>
-                <TouchableOpacity style={caminho === '/' ? styles.botaoAtivo : styles.botao}>
+            <Link href={"/familia/dados"} asChild>
+                <TouchableOpacity style={caminho?.startsWith('/familia/dados') ? styles.botaoAtivo : styles.botao}>
                     <Ionicons
                         name="analytics"
                         size={14}
-                        color={caminho === '/' ? '#ffffff' : '#1D1252'}
+                        color={caminho?.startsWith('/familia/dados') ? '#ffffff' : '#1D1252'}
                     />
-                    <Text style={caminho === '/' ? styles.textAtivo : styles.text}>
+                    <Text style={caminho?.startsWith('/familia/dados') ? styles.textAtivo : styles.text}>
                         Dados
                     </Text>
                 </TouchableOpacity>
